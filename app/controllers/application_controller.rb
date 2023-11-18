1380363730463c2bd724a0491197120f206df30a
nao52 <naoki.52abc@icloud.com>
2023-11-18 10:32:26 +0900

    ローカル環境で環境変数を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :require_login
   add_flash_types :success, :danger
 
+  def google_client_id
+    @google_client_id = Settings.google_client_id
+  end
+
   private
 
   def not_authenticated
--- app/controllers/google_login_api_controller.rb
+++ app/controllers/google_login_api_controller.rb
@@ -2,11 +2,13 @@ class GoogleLoginApiController < ApplicationController
   require 'googleauth/id_tokens/verifier'
 
   protect_from_forgery except: :callback
+
   skip_before_action :require_login
   before_action :verify_g_csrf_token
+  before_action :google_client_id
 
   def callback
-    Google::Auth::IDTokens.verify_oidc(params[:credential], aud: "938607287835-1qfde5lo2p693gb7qtsa9nrt5ht4qt83.apps.googleusercontent.com")
+    Google::Auth::IDTokens.verify_oidc(params[:credential], aud: @google_client_id)
     redirect_to login_path, success: 'ログインしました'
   end
 
